2fd4c6c4ecaa3b63f40715efef7f18eed5348d4e
Paul Melnikow <email@paulmelnikow.com>
2018-11-15 17:20:29 -0500

    Require the JSON file to play well with bundlers

--- packages/anafanafo/index.js
+++ packages/anafanafo/index.js
@@ -1,9 +1,8 @@
 'use strict'
 
-const path = require('path')
-const { loadConsumerSync } = require('char-width-table-consumer')
+const { createConsumer } = require('char-width-table-consumer')
 
-const dataPath = path.join(__dirname, 'data', 'widths.json')
-const consumer = loadConsumerSync(dataPath)
+const data = require('./data/widths.json')
+const consumer = createConsumer(data)
 
 module.exports = (text, options) => consumer.widthOf(text, options)

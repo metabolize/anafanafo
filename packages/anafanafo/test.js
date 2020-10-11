51a109d5dcbeb53ee9984b66dbf7fedfd5638ab3
Paul Melnikow <email@paulmelnikow.com>
2020-10-11 16:55:16 -0400

    Social badge uses bold 11px Helvetica, not bold 11px Verdana

--- packages/anafanafo/test.js
+++ packages/anafanafo/test.js
@@ -5,7 +5,7 @@ const anafanafo = require('.')
 
 describe('anafanafo', () => {
   it('Measures 11px Verdana', () => {
-    const options = { size: '10px', weight: 'normal' }
+    const options = { font: '10px Verdana' }
     expect(anafanafo('m', options)).to.equal(9.73)
     expect(anafanafo('v1.2.511', options)).to.be.within(
       44.98 - 0.1,
@@ -14,7 +14,7 @@ describe('anafanafo', () => {
   })
 
   it('Measures bold 10px Verdana', () => {
-    const options = { size: '10px', weight: 'bold' }
+    const options = { font: 'bold 10px Verdana' }
     expect(anafanafo('m', options)).to.be.within(10.59 - 0.1, 10.59 + 0.1)
     expect(anafanafo('v1.2.511', options)).to.be.within(
       49.28 - 0.1,
@@ -23,7 +23,7 @@ describe('anafanafo', () => {
   })
 
   it('Measures 11px Verdana', () => {
-    const options = { size: '11px', weight: 'normal' }
+    const options = { font: '11px Verdana' }
     expect(anafanafo('m', options)).to.equal(10.7)
     expect(anafanafo('v1.2.511', options)).to.be.within(
       49.48 - 0.1,
@@ -31,8 +31,8 @@ describe('anafanafo', () => {
     )
   })
 
-  it('Measures bold 11px Verdana', () => {
-    const options = { size: '11px', weight: 'bold' }
+  it('Measures bold 11px Helvetica', () => {
+    const options = { font: 'bold 11px Helvetica' }
     expect(anafanafo('m', options)).to.equal(11.64)
     expect(anafanafo('v1.2.511', options)).to.be.within(54.2 - 0.1, 54.2 + 0.1)
   })
